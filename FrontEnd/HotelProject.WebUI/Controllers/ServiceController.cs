001644a4444e131f548a5af69f66c9dd1c1f9dea
Muhammed Ferit Simsek <cpt.mfs@gmail.com>
2024-01-09 16:36:40 +0300

    ServiceUpdate_FileUpload

--- FrontEnd/HotelProject.WebUI/Controllers/ServiceController.cs
+++ FrontEnd/HotelProject.WebUI/Controllers/ServiceController.cs
@@ -1,9 +1,11 @@
 ﻿using HotelProject.Entity.Concrete;
 using HotelProject.WebUI.Dtos.ServiceDto;
 using HotelProject.WebUI.Models.Staff;
+using HotelProject.WebUI.Models.Testimonial;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace HotelProject.WebUI.Controllers
@@ -37,7 +39,7 @@ namespace HotelProject.WebUI.Controllers
         {
             return View();
         }
-        [HttpPost]
+        [HttpPost]        
         public async Task<IActionResult> AddService(CreateServiceDto add)
         {
             if (!ModelState.IsValid) return View();
@@ -69,6 +71,67 @@ namespace HotelProject.WebUI.Controllers
             }
 
             return View(add);
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateService(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"http://localhost:5282/api/Service/{id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateServiceDto>(jsonData);
+
+                return View(values);
+            }
+            return View();
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UpdateService(UpdateServiceDto model)
+        {
+            if (!ModelState.IsValid) return View();
+
+            var client = _httpClientFactory.CreateClient();
+
+            if (model.ServiceIconUpload != null && model.ServiceIconUpload.Length > 0)
+            {
+                // Dosyayı kaydetme işlemi
+                string fileName = GetUniqueFileName(model.ServiceIconUpload.FileName);
+                string filePath = Path.Combine(_webHostEnvironment.WebRootPath, "Images", fileName);
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await model.ServiceIconUpload.CopyToAsync(fileStream);
+                }
+
+                // Service modelindeki ServiceIcon özelliğini dosya adıyla güncelleme
+                model.ServiceIcon = fileName;
+            }
+
+            var jsonData = JsonConvert.SerializeObject(model);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("http://localhost:5282/api/Service", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        public async Task<IActionResult> DeleteService(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+
+            var responseMessage = await client.DeleteAsync($"http://localhost:5282/api/Service/{id}");
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
         }
         private string GetUniqueFileName(string fileName)
         {
--- FrontEnd/HotelProject.WebUI/Dtos/ServiceDto/UpdateServiceDto.cs
+++ FrontEnd/HotelProject.WebUI/Dtos/ServiceDto/UpdateServiceDto.cs
@@ -4,8 +4,10 @@ namespace HotelProject.WebUI.Dtos.ServiceDto
 {
     public class UpdateServiceDto
     {
-        [Required(ErrorMessage = "Hizmet ikon linki giriniz.")]
-        public string ServiceIcon { get; set; }
+        public int ServiceID { get; set; }
+
+        public string? ServiceIcon { get; set; }
+        public IFormFile ServiceIconUpload { get; set; }
         [Required(ErrorMessage = "Hizmet başlığı giriniz.")]
         [StringLength(100, ErrorMessage = "Hizmet başlığı en fazla 100 karakter olabilir.")]
         public string Title { get; set; }
